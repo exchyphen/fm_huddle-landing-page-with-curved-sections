e2140487f2d5322c2a7dee9f2d1f867596ff17ee
exchyphen <exc.hyphen@gmail.com>
2024-03-19 12:45:00 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <footer>
+        <p className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
