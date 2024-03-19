c67fad5da9cc7813ab5d7a444aee6bee5876f805
exchyphen <exc.hyphen@gmail.com>
2024-03-19 16:24:00 -0700

    complete mobile

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,37 @@ import IconMessages from "./assets/images/icon-messages.svg";
 import ImgGrowTogether from "./assets/images/illustration-grow-together.svg";
 import ImgFlowingConversations from "./assets/images/illustration-flowing-conversation.svg";
 import ImgYourUsers from "./assets/images/illustration-your-users.svg";
+import IconPhone from "./assets/images/icon-phone.svg";
+import IconEmail from "./assets/images/icon-email.svg";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+
+  /* helper functions */
+  function isEmail(input) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input);
+  }
+
+  /* handlers */
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!isEmail(email)) {
+      setError(true);
+      return;
+    }
+    setError(false);
+  };
+
   return (
     <>
       <nav className="nav block--padding">
         <img className="nav__logo" src={Logo} alt="logo"></img>
-        <button className="button__round--light">Try it Free</button>
+        <button className="button__round--light nav__button">
+          Try it Free
+        </button>
       </nav>
 
       <header className="header block--padding">
@@ -64,7 +88,7 @@ function App() {
 
       <main className="main">
         <article className="feature1">
-          <div className="feature__top-bg bg"></div>
+          <div className="feature__top-bg-1 bg"></div>
           <section className="feature__section block--padding">
             <article className="section__content block--max-width">
               <hgroup className="section__hgroup">
@@ -84,12 +108,12 @@ function App() {
               </div>
             </article>
           </section>
-          <div className="feature__bot-bg bg"></div>
+          <div className="feature__bot-bg-1 bg"></div>
         </article>
 
         <article className="feature2">
           <section className="feature__section feature__section--light block--padding">
-            <article className="section__content block--max-width">
+            <article className="section__content section__content--flip block--max-width">
               <div className="section__img-wrapper">
                 <img
                   className="section__img"
@@ -110,7 +134,7 @@ function App() {
         </article>
 
         <article className="feature3">
-          <div className="feature__top-bg bg"></div>
+          <div className="feature__top-bg-2 bg"></div>
           <section className="feature__section block--padding">
             <article className="section__content block--max-width">
               <hgroup className="section__hgroup">
@@ -130,7 +154,12 @@ function App() {
               </div>
             </article>
           </section>
-          <div className="feature__bot-bg bg"></div>
+          <div className="feature__bot-bg-2 bg"></div>
+        </article>
+
+        <article className="cta block--padding">
+          <h2 className="text--headings">Ready To Build Your Community?</h2>
+          <button className="button__round--dark">Get Started For Free</button>
         </article>
       </main>
 
@@ -138,10 +167,35 @@ function App() {
         <div className="footer__bg bg"></div>
         <article className="footer__container block--padding">
           <section className="footer__content">
+            <img className="footer__logo" src={Logo} alt="footer logo"></img>
+
+            <div className="footer__contact">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+              </p>
+              <div className="contact__item">
+                <img
+                  className="contact__img"
+                  src={IconPhone}
+                  alt="phone icon"
+                ></img>
+                <p>Phone: +1-543-123-4567</p>
+              </div>
+              <div className="contact__item">
+                <img
+                  className="contact__img"
+                  src={IconEmail}
+                  alt="email icon"
+                ></img>
+                <p>example@huddle.com</p>
+              </div>
+            </div>
+
             <div className="social__container">
-              <i className="fa-brands fa-facebook-f"></i>
-              <i className="fa-brands fa-instagram"></i>
-              <i className="fa-brands fa-twitter"></i>
+              <i className="fa-brands fa-square-facebook fa-2xl social__icon"></i>
+              <i className="fa-brands fa-instagram fa-2xl social__icon"></i>
+              <i className="fa-brands fa-square-twitter fa-2xl social__icon"></i>
             </div>
 
             <p className="attribution">
@@ -159,7 +213,38 @@ function App() {
               .
             </p>
           </section>
-          <section></section>
+
+          <section className="newsletter__container">
+            <hgroup className="newsletter__hgroup">
+              <h3 className="newsletter__h3 ">newsletter</h3>
+              <div>
+                To recieve tips on how to grow your community, sign up to our
+                weekly newsletter. We’ll never send you spam or pass on your
+                email address
+              </div>
+            </hgroup>
+            <form className="email__container">
+              <div className="input__container">
+                <input
+                  className={"email" + (error ? " email--error" : "")}
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                ></input>
+                {error ? (
+                  <p className="error-msg">Check your email please</p>
+                ) : null}
+              </div>
+              <button
+                className="email__button"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                Subscribe
+              </button>
+            </form>
+          </section>
         </article>
       </footer>
     </>
